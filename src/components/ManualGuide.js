a91b8d88a85f457d763c67ca86de33e32b1a054e
João Silva <joaosilva7875@gmail.com>
2023-04-25 23:29:19 +0200

    fully built the samsung calibration guide page

--- src/components/ManualGuide.js
+++ src/components/ManualGuide.js
@@ -16,29 +16,28 @@ export const ManualGuide = ({ signUserOut }) => {
       <div className='manual__container'>
         <h1>!!!TEST!!! !!!MANUAL GUIDE!!!</h1>
         <br></br>
-        <h3 className='color__black'>First, start the first reading as soon as you get it out of the box</h3>
+        <h3 className='color__white'>Fill in all the calman information, select "color management" and start the first reading</h3>
         <h2>Preparation</h2>
         <ul>
-          <li className='color__black'>Change mode selection from standard to movie if It's Samsumg, or Expert day bright if LG</li>
-          <li className='color__black'>Turn off Energy Saving</li>
-          <li className='color__black'>Turn off filter functions such as noise editing, motionflow, local dimming, etc...</li>
+          <li className='color__white'>Change mode selection from standard to movie </li>
+          <li className='color__white'>Turn off Energy Saving</li>
+          <li className='color__white'>Turn off filter functions such as noise editing, motionflow, local dimming, etc...</li>
         </ul>
         <h2>Calibration</h2>
         <ul>
-          <li className='color__black'>Luminance</li>
-          <li className='color__black'>Contrast</li>
-          <li className='color__black'>Black Level</li>
-          <li className='color__black'>Gamma</li>
-          <li className='color__black'>Luminance</li>
-          <li className='color__black'>White Balance</li>
-          <li className='color__black'>Color Management</li>
-          <li className='color__black'>Sharpness</li>
+          <li className='color__white'>Contrast</li>
+          <li className='color__white'>Black Level</li>
+          <li className='color__white'>Gamma</li>
+          <li className='color__white'>Luminance</li>
+          <li className='color__white'>White Balance</li>
+          <li className='color__white'>Color Management</li>
+          <li className='color__white'>Sharpness</li>
         </ul>
         <h2>Final Steps</h2>
         <ul>
-          <li className='color__black'>Final Reading</li>
-          <li className='color__black'>Port Application</li>
-          <li className='color__black'>Change Language</li>
+          <li className='color__white'>Final Reading</li>
+          <li className='color__white'>Port Application</li>
+          <li className='color__white'>Change Language</li>
         </ul>
       </div>
     </div>
--- src/components/SamsungGuide.js
+++ src/components/SamsungGuide.js
@@ -1,19 +1,71 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/SamsungGuide.css";
+import SamsungBox from "../images/samsungbox.jpeg";
 
 export const SamsungGuide = ({ signUserOut }) => {
   return (
-    <div className='samsung__container'>
-      <div>
-        <div className='sign__out'>
-          <button className='signOut--sendMessage__button' onClick={signUserOut}>
-            Sign Out
-          </button>
-          <Link to='/'>
-            <button className='signOut--sendMessage__button'>Home</button>
-          </Link>
-        </div>
+    <div>
+      <div className='sign__out'>
+        <button className='signOut--sendMessage__button' onClick={signUserOut}>
+          Sign Out
+        </button>
+        <Link to='/'>
+          <button className='signOut--sendMessage__button'>Home</button>
+        </Link>
+      </div>
+      <div className='samsung__container'>
         <h1>SAMSUNG GUIDE</h1>
+        <div className='row__container'>
+          <div className='p__container'>
+            <ol>
+              <li>
+                <span className='li__color'>Turn on the TV and go through all the steps until network connection</span>
+              </li>
+              <li>
+                <span className='li__color'>When it gets to network connection, wait 1 to 2 seconds and cancel</span>
+              </li>
+              <li>
+                <span className='li__color'>Get the IP Address from the TV and type it on the laptop</span>
+              </li>
+              <li>
+                <span className='li__color'>Finish the setup and go to HDMI 1</span>
+              </li>
+              <li>
+                <span className='li__color'>Start first reading</span>
+              </li>
+              <li>
+                <span className='li__color' n>
+                  After the reading is finished, go to settings
+                </span>
+              </li>
+              <li>
+                <span className='li__color'>Go to network settings and enable IP remote</span>
+              </li>
+              <li>
+                <span className='li__color'>On the laptop, select "Find TV"</span>
+              </li>
+              <li>
+                <span className='li__color'>Back to the settings, turn off the Energy Saving option</span>
+              </li>
+              <li>
+                <span className='li__color'>
+                  Go to Picture settings &#8594; Expert settings &#8594; Put Sharpness on 5 and on colors, select custom
+                </span>
+              </li>
+              <li>
+                <span className='li__color'>Go back to HDMI 1 and start the second reading</span>
+              </li>
+              <li>
+                <span className='li__color'>Create the TV report</span>
+              </li>
+              <li>
+                <span className='li__color'>Change the language to dutch</span>
+              </li>
+            </ol>
+            <img className='samsung__box' alt='Samsung TV Box' src={SamsungBox} />
+          </div>
+        </div>
       </div>
     </div>
   );
