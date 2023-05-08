639709e8b1a753ca1bc7aa05073806ee47ab780a
João Silva <joaosilva7875@gmail.com>
2023-05-08 16:43:15 +0200

    wrote phillips tv short guide (2 steps missing)

--- src/components/PhillipsGuide.js
+++ src/components/PhillipsGuide.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import PhillipsBox from "../images/phillipsTV.png";
 import PhillipsReport from "../documents/Phillips_Report.pdf";
 import PdfDownloader from "./PdfDownloader";
+import "../styles/PhillipsGuide.css";
 
 export const PhillipsGuide = ({ signUserOut }) => {
   const [shortGuide, setShortGuide] = useState(false);
@@ -47,47 +48,78 @@ export const PhillipsGuide = ({ signUserOut }) => {
           <h2 style={{ textAlign: "center" }}>Short Guide</h2>
           <div style={{ marginTop: "1rem" }}>
             <div className='p__container'>
-              <ol>
-                <li>
-                  <span className='li__color'>Turn on the TV and select the language/country</span>
-                </li>
-                <li>
-                  <span className='li__color'>Select "exit first user" and confirm by selecting "yes"</span>
-                </li>
-                <li>
-                  <span className='li__color'>Open settings, go to picture mode and select "Expert Bright"</span>
-                </li>
-                <li>
-                  <span className='li__color'>Turn off energy saving</span>
-                </li>
-                <li>
-                  <span className='li__color'>Go back to HDMI 1 and start the first reading</span>
-                </li>
-                <li>
-                  <span className='li__color'>After the reading is finished, on the laptop click "Find TV"</span>
-                </li>
-                <li>
-                  <span className='li__color'>When the unique code shows up on the TV screen, type it into the laptop</span>
-                </li>
-                <li>
-                  <span className='li__color'>Leave the only option selected as "Expert Day" and start the last reading</span>
-                </li>
-                <li>
-                  <span className='li__color'>After the reading is finished, create the report</span>
-                </li>
-                <li>
-                  <span className='li__color'>Go to every HDMI and change it's picture setting to "Expert Bright"</span>
-                </li>
-                <li>
-                  <span className='li__color'>Go back to HDMI 1 and start the second reading</span>
-                </li>
-                <li>
-                  <span className='li__color'>Go to advanced settings and select "Apply to all inputs"</span>
-                </li>
-                <li>
-                  <span className='li__color'>Change the language to dutch</span>
-                </li>
-              </ol>
+              <div className='short__guide__container'>
+                <ol>
+                  <li>
+                    <span className='li__color'>
+                      The Phillips TV has a different step than any other TV, we connect the HDMI cable, the power cable, but we will only connect the
+                      LAN cable later on
+                    </span>
+                  </li>
+                  <li>
+                    <span className='li__color'>Turn the TV on and select "English" or "Dutch" as the language</span>
+                  </li>
+                  <li>
+                    <span className='li__color'>Skip the Pairing by pressing "OK" on the TV remote</span>
+                  </li>
+                  <li>
+                    <span className='li__color'>Select "Later"</span>
+                  </li>
+                  <li>
+                    <span className='li__color'>Accept Google's Terms of Service &#8594; Accept Google Services</span>
+                  </li>
+                  <li>
+                    <span className='li__color'>Press the "right" arrow on the TV remote to navigate through this screen</span>
+                  </li>
+                  <li>
+                    <span className='li__color'>Select "The Netherlands" &#8594; "Home Use" &#8594; "Later" &#8594; "Continue"</span>
+                  </li>
+                  <li>
+                    <span className='li__color'>When it shows "Updating Apps" on the screen, we can now connect the LAN cable to the TV</span>
+                  </li>
+                  <li>
+                    <span className='li__color'>Select "Noted" &#8594; "Finish"</span>
+                  </li>
+                  <li>
+                    <span className='li__color'>Once the TV is ready, press the "Sources/Input" button on the remote and go to HDMI1</span>
+                  </li>
+                  <li>
+                    <span className='li__color'>
+                      Start the first reading &#40;if a pop-up appears regarding the "Quick Picture Settings", select "Later"&#41;
+                    </span>
+                  </li>
+                  <li>
+                    <span className='li__color'>
+                      Once the reading is finished, press the "Menu" button on the TV remote and select "Frequent Settings"
+                    </span>
+                  </li>
+                  <li>
+                    <span className='li__color'>
+                      Scroll all the way down and select "All Settings" &#8594; "Picture" &#40;if a "ECO Reminder" pop-up appears, select "OK"&#41;
+                    </span>
+                  </li>
+                  <li>
+                    <span className='li__color'>
+                      Select "AI Picture Style" &#8594; "Calman" &#40;once again if "ECO Reminder" shows up, press "OK"&#41;
+                    </span>
+                  </li>
+                  <li>
+                    <span className='li__color'>
+                      Now that "Calman" is selected, press the green button on your TV Remote. &#40;There are 4 colored buttons. Red, green, yellow
+                      and blue&#41;
+                    </span>
+                  </li>
+                  <li>
+                    <span className='li__color'>Select "Auto" and type the IP Address on the laptop &#8594; "Find Phillips TV"</span>
+                  </li>
+                  <li>
+                    <span className='li__color'>Once it's connected, leave only "SDR Calibration" selected, and click "Start AutoCal"</span>
+                  </li>
+                  <li>
+                    <span className='li__color'>!!!TO-DO: FINISH WRITING LAST STEPS.!!! </span>
+                  </li>
+                </ol>
+              </div>
               <img className='lg__box' alt='Sony TV Box' src={PhillipsBox} />
             </div>
           </div>
